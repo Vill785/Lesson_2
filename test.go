d66a55a43fcbe66c8fa821e791bb4af2af9ab3b7
Vlad <79089690+Vill785@users.noreply.github.com>
2024-05-07 13:05:02 +0300

    Update test.go

--- test.go
+++ test.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
-	var a int
-	fmt.Scan(&a)
-	x := a / 100
-	y := a / 10 % 10
-	z := a % 10
-	fmt.Print(strconv.Itoa(z) + strconv.Itoa(y) + strconv.Itoa(x))
+	a := 200
+	b := &a
+	*b++
+	c := &b
+	**c++ // указатель на указатель
+	fmt.Println(a)
 }
